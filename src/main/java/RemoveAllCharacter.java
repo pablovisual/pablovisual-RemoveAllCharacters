915c458ac3316d4a4f5fa006d03758cbee2b4f08
Pablo Sandoval <pablo.a.sandoval01@utrgv.edu>
2023-03-01 21:43:41 -0600

    coding challenge complete

--- src/main/java/RemoveAllCharacter.java
+++ src/main/java/RemoveAllCharacter.java
@@ -1,17 +1,31 @@
 
 public class RemoveAllCharacter {
-    /**
-     * Return a String that has all the character of a String removed.
-     * You could either use a for loop to build a new String that excludes a character, or you could cleverly use
-     * the replaceAll method of String to replace a character with a blank space.
-     *
-     * Here are the docs for String: https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
-     *
-     * @param str A string.
-     * @param ch A string containing a single character which should be removed from str.
-     * @return str, without any characters of type ch.
-     */
-    public String removeAll(String str, String ch){
-        return null;
-    }
+  /**
+   * Return a String that has all the character of a String removed.
+   * You could either use a for loop to build a new String that excludes a character, or you could cleverly use
+   * the replaceAll method of String to replace a character with a blank space.
+   *
+   * Here are the docs for String: https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+   *
+   * @param str A string.
+   * @param ch A string containing a single character which should be removed from str.
+   * @return str, without any characters of type ch.
+   */
+  public String removeAll(String str, String ch) {
+    return str.replaceAll(ch, "");
+
+    ///using a loop
+        /*String str2 = "";
+        for (int index = 0; index < str.length(); index++) {
+            if(str.charAt(index) == ch.charAt(0)) {
+                continue;
+            }
+
+            else {
+                str2 += str.charAt(index);
+            }
+        }
+
+        return str2;*/
+  }
 }
